e7086c8b1652eedbe8edcf6a1d4ad3f7640d1548
Chun-Yu Wang <wicanr2@gmail.com>
2014-12-31 22:35:02 +0800

    add fed-mr execution result and log

--- fed_script/runFedW_newMR.sh
+++ fed_script/runFedW_newMR.sh
@@ -2,5 +2,5 @@
 cd /home/hpds/zien/FedMR-hadoop-2.4.1-time/hadoop-2.4.1
 bin/hadoop fs -rm -r _distcp_logs_*
 bin/hadoop fs -rm -r new_wc_out 
-bin/hadoop jar fed_task/word_count_new_api.jar -Dfed=true -Dfedconf=fed_task/wc_new_api.xml wordcount2 test1 new_wc_out &> test.txt
+bin/hadoop jar fed_task/word_count_new_api.jar -Dfed=true -Dfedconf=fed_task/wc_new_api.xml wordcount2 6G new_wc_out
 bin/hadoop fs -rm -r _distcp_logs_*
