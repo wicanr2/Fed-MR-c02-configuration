c638bb77943af75960377c3fcd3c3db505a0b76a
Chun-Yu Wang <wicanr2@gmail.com>
2014-12-31 20:57:32 +0800

    add new script

--- fed_script/runFed_grep.sh
+++ fed_script/runFed_grep.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cd /home/hpds/zien/FedMR-hadoop-2.4.1-time/hadoop-2.4.1
-bin/hadoop fs -rm -r _distcp_logs_*
+#bin/hadoop fs -rm -r _distcp_logs_*
 bin/hadoop fs -rm -r grepOut 
-bin/hadoop jar hadoop-mapreduce-examples-2.4.1.jar grep -Dfed=true -Dfedconf=fed_task/grep.xml 6G grepOut xx 
-bin/hadoop fs -rm -r _distcp_logs_*
-#run local 
-bin/hadoop jar hadoop-mapreduce-examples-2.4.1.jar grep 6G grepOutLocal unpredict
+bin/hadoop fs -rm -r grepSortedOut 
+# collect data from region cloud 
+bin/hadoop jar hadoop-mapreduce-examples-2.4.1.jar grep -Dfed=true -Dfedconf=fed_task/grep.xml 6G grepOut unpredict
+#bin/hadoop fs -rm -r _distcp_logs_*
+# run on local to sort the result
+bin/hadoop jar hadoop-mapreduce-examples-2.4.1.jar p2grep grepOut grepSortedOut
